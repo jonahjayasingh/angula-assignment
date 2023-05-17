97f707d2187a996353025ba6ca38e2be7241684a
Jonah Jayasingh <81973624+jonahjayasingh@users.noreply.github.com>
2023-05-17 14:34:36 +0100

    Add files via upload

---
+++ 2-2assignment/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '2-2assignment';
+
+  username = "";
+  allow = true
+
+  onClickRest(){
+    this.username= ""
+    this.allow=true
+  }
+
+  updateacces(){
+    this.allow= false
+  }
+  
+}
